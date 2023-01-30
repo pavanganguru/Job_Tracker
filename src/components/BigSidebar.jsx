0588de020b90d94132e99f5f861fab3a5ee41bb6
pavan ganguru <pavanganguru999@gmail.com>
2023-01-30 14:58:01 -0600

    Dash Board design -> navbar, bigsidebar, smallsidebar

---
+++ src/components/BigSidebar.jsx
@@ -0,0 +1,11 @@
+import Wrapper from "../assets/wrappers/BigSidebarCSS";
+
+const BigSidebar = () => {
+  return (
+    <Wrapper>
+      <h2>Big Side Bar</h2>
+    </Wrapper>
+  );
+};
+
+export default BigSidebar;
---
+++ src/components/SmallSidebar.jsx
@@ -0,0 +1,10 @@
+import Wrapper from "../assets/wrappers/SmallSidebarCSS";
+const SmallSidebar = () => {
+  return (
+    <Wrapper>
+      <h2>Small Side Bar</h2>
+    </Wrapper>
+  );
+};
+
+export default SmallSidebar;
