41359d2387850701f57c4647d37a08ba63973b81
pavan ganguru <pavanganguru999@gmail.com>
2023-02-01 14:27:41 -0600

    job component completed

--- src/components/Job.jsx
+++ src/components/Job.jsx
@@ -1,5 +1,59 @@
-const Job = () => {
-  return <h2>job</h2>;
+import { FaLocationArrow, FaBriefcase, FaCalendarAlt } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import Wrapper from "../assets/wrappers/JobCSS";
+import { useDispatch } from "react-redux";
+import JobInfo from "./JobInfo";
+import moment from "moment/moment";
+
+const Job = ({
+  _id,
+  company,
+  position,
+  jobLocation,
+  jobType,
+  status,
+  createdAt,
+}) => {
+  const dispatch = useDispatch();
+  const date = moment(createdAt).format("MMM Do, YYYY");
+
+  return (
+    <Wrapper>
+      <header>
+        <div className="main-icon">{company.charAt(0)}</div>
+        <div className="info">
+          <h5>{position}</h5>
+          <p>{company}</p>
+        </div>
+      </header>
+      <div className="content">
+        <div className="content-center">
+          <JobInfo icon={<FaLocationArrow />} text={jobLocation} />
+          <JobInfo icon={<FaCalendarAlt />} text={date} />
+          <JobInfo icon={<FaBriefcase />} text={jobType} />
+          <div className={`status ${status}`}>{status}</div>
+        </div>
+        <footer>
+          <div className="actions">
+            <Link
+              to="/add-job"
+              className="btn edit-btn"
+              onClick={() => console.log("edit-job")}
+            >
+              Edit{" "}
+            </Link>
+            <button
+              type="button"
+              className="btn delete-btn"
+              onClick={() => console.log("delete-job")}
+            >
+              Delete
+            </button>
+          </div>
+        </footer>
+      </div>
+    </Wrapper>
+  );
 };
 
 export default Job;
--- src/features/allJobs/allJobSlice.jsx
+++ src/features/allJobs/allJobSlice.jsx
@@ -31,7 +31,6 @@ export const getAllJobs = createAsyncThunk(
           Authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
         },
       });
-      console.log(response.data);
       return response.data;
     } catch (error) {
       return thunkAPI.rejectWithValue("there was an error");
