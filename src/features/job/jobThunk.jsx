f99cf85f6f21f1e635b3ee1974303474b238a3ec
pavan ganguru <pavanganguru999@gmail.com>
2023-02-01 15:47:51 -0600

    refactored authHeader in jobThunk #approach1

---
+++ src/features/job/jobThunk.jsx
@@ -0,0 +1,50 @@
+import { showLoading, hideLoading, getAllJobs } from "../allJobs/allJobSlice";
+import fetch from "../../utils/axios";
+import { clearValues } from "./jobSlice";
+import { logoutUser } from "../user/userSlice";
+
+const authHeader = (thunkAPI) => {
+  return {
+    headers: {
+      Authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+    },
+  };
+};
+
+export const createJObThunk = async (job, thunkAPI) => {
+  try {
+    const response = await fetch.post("/jobs", job, authHeader(thunkAPI));
+    thunkAPI.dispatch(clearValues());
+    return response.data;
+  } catch (error) {
+    if (error.response.status === 401) {
+      thunkAPI.dispatch(logoutUser());
+      return thunkAPI.rejectWithValue("Unauthorized!! Logging Out...");
+    }
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+export const deleteJObThunk = async (jobId, thunkAPI) => {
+  thunkAPI.dispatch(showLoading());
+  try {
+    const response = await fetch.delete(`/jobs/${jobId}`, authHeader(thunkAPI));
+    thunkAPI.dispatch(getAllJobs());
+    return response.data.msg;
+  } catch (error) {
+    thunkAPI.dispatch(hideLoading());
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
+export const editJObThunk = async ({ jobId, job }, thunkAPI) => {
+  try {
+    const response = await fetch.patch(
+      `/jobs/${jobId}`,
+      job,
+      authHeader(thunkAPI)
+    );
+    thunkAPI.dispatch(clearValues());
+    return response.data;
+  } catch (error) {
+    return thunkAPI.rejectWithValue(error.response.data.msg);
+  }
+};
